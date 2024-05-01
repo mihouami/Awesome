b62e346b278c67daeae3051ad3deec1082546cce
Minou <am.mihoubi@gmail.com>
2024-05-01 12:26:57 +0100

    04 - POSTS TAGS

--- a_core/urls.py
+++ a_core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from a_posts.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +13,6 @@ urlpatterns = [
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post'),
     
-]
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
